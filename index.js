d69be8d1f4d5780bab5d0bc1864a91a880227286
Angel Oyaga <santioyaga10@gmail.com>
2025-07-10 19:27:35 -0500

    diseño inicial de página principal finalizado

---
+++ index.js
@@ -0,0 +1,10 @@
+// Menú 
+const menuBtn = document.getElementById('menu-btn');
+const hamburguerBtn = document.getElementById('burguer-menu');
+
+if (menuBtn && hamburguerBtn) {
+  menuBtn.addEventListener("click", () => {
+    hamburguerBtn.classList.toggle("hidden");
+  });
+}
+
