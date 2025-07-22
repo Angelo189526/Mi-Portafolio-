1a5ef1813a39c71c380a6b5866977290a3fcfbeb
Angel Oyaga <santioyaga10@gmail.com>
2025-07-21 21:04:13 -0500

    Layout general y DarkMode terminado

--- DarkMode.js
+++ DarkMode.js
@@ -1,15 +1,26 @@
-// Dark Mode
-tailwind.config = {
-    darkMode: 'class', 
-  }
-  
-  const toggleBtn = document.getElementById("toggle-theme");
-  
-  toggleBtn.addEventListener("click", () => {
-    document.documentElement.classList.toggle("dark");
-  
-  
+window.addEventListener("DOMContentLoaded", function () {
+    const toggleBtn = document.getElementById("toggle-theme");
+
+    if (localStorage.getItem("theme") === "dark") {
+      document.documentElement.classList.add("dark");
+    }
+
+    toggleBtn.addEventListener("click", () => {
+      document.documentElement.classList.toggle("dark");
+
+      if (document.documentElement.classList.contains("dark")) {
+        localStorage.setItem("theme", "dark");
+      } else {
+        localStorage.setItem("theme", "light");
+      }
+    });
   });
-  
+
+  // Configuración de Tailwind para activar el modo oscuro por clase
+  tailwind.config = {
+    darkMode: 'class',
+  }
+
+
   
   
